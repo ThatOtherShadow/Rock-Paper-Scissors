b75179a2e01549b7f0c818847d0c0c8e805c691a
Shadow <43815633+Jirka1974@users.noreply.github.com>
2023-11-30 17:36:52 +0100

    Implemented main menu

--- src/main/kotlin/main_controller.kt
+++ src/main/kotlin/main_controller.kt
@@ -1,7 +1,29 @@
-class main_controller {
+import com.sun.java.swing.action.HelpMenu
 
+class main_controller() {
+    val ioControl = io_control()
+    val RPSLogic = rps_logic()
+    val HelpMenu = help_menu()
 
-    fun main() {
-        println("Enter 1 to play, Enter 2 for help, Enter 3, X or EXIT to exit")
+    var main_menu_input = "0"
+
+    fun game_control() {
+        println("Enter 1 to play, Enter 2 for help, Enter 3, x or EXIT to exit")
+        main_menu_input = readln()
+        if (main_menu_input == "1") {
+            //TODO Implement RPS Start Logic
+        }
+        else if (main_menu_input == "2") {
+            //TODO Open Help Menu
+        }
+        else if (main_menu_input.lowercase() in arrayOf("3", "x", "exit")) {
+            //TODO Implement Exit Logic
+        }
     }
+}
+
+fun main() {
+    val mainController = main_controller()
+
+    mainController.game_control()
 }
