497d95243df4de9ef9eebcd2b8d6cedbc3169ecd
Shadow <43815633+Jirka1974@users.noreply.github.com>
2023-11-30 17:15:45 +0100

    initial commit

---
+++ src/main/kotlin/main_controller.kt
@@ -0,0 +1,7 @@
+class main_controller {
+
+
+    fun main() {
+        println("Enter 1 to play, Enter 2 for help, Enter 3, X or EXIT to exit")
+    }
+}
---
+++ src/main/kotlin/rps_logic.kt
@@ -0,0 +1,5 @@
+class rps_logic() {
+
+
+
+}
