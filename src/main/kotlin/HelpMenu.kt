3c018ce9082612d508aea2905713e63ee733275d
Shadow <43815633+Jirka1974@users.noreply.github.com>
2023-12-01 17:06:53 +0100

    Implemented basic RPS logic

--- src/main/kotlin/HelpMenu.kt
+++ src/main/kotlin/HelpMenu.kt
@@ -3,9 +3,15 @@ class HelpMenu {
 
     fun showMenu() {
         println("""
-        NOT IMPLEMENTED YET
+        Enter 1, R, or Rock to choose rock
+
+        Enter 2, P, or Paper to choose paper
+
+        Enter 3, S, or Scissors to choose scissors
+        
+
+        Press enter to return...
         """.trimIndent())
-        println("Press enter to return.")
         readln()
     }
 }
--- src/main/kotlin/RPSLogic.kt
+++ src/main/kotlin/RPSLogic.kt
@@ -1,6 +1,85 @@
+import kotlin.random.Random
+
 class RPSLogic {
 
     fun startGame() {
-        println("NOT IMPLEMENTED YET")
+        val computerPick = computerPick()
+        val userIn = getInput()
+        when (userIn) {
+            1 -> {
+                when (computerPick) {
+                    1 -> {
+                        tie(1)
+                    }
+                    2 -> {
+                        playerWin(1)
+                    }
+                    else -> {
+                        playerLose(1)
+                    }
+                }
+            }
+            2 -> {
+                when (computerPick) {
+                    1 -> {
+                        playerWin(2)
+                    }
+                    2 -> {
+                        tie(2)
+                    }
+                    else -> {
+                        playerLose(2)
+                    }
+                }
+            }
+            3 -> {
+                when (computerPick) {
+                    1 -> {
+                        playerLose(3)
+                    }
+                    2 -> {
+                        playerWin(3)
+                    }
+                    else -> {
+                        tie(3)
+                    }
+                }
+            }
+        }
+        }
+    }
+
+    private fun computerPick(): Int {
+        return Random.nextInt(1, 3)
+    }
+
+    fun getInput(): Int {
+        while (true) {
+            val userInput = readln()
+            if (userInput.lowercase() in arrayOf("1", "r", "rock")) {
+                return 1
+            }
+            else if (userInput.lowercase() in arrayOf("2", "p", "paper")) {
+                return 2
+            }
+            else if (userInput.lowercase() in arrayOf("3", "s", "scissors")) {
+                return 3
+            }
+            else {
+                println("Invalid input.")
+            }
+        }
+    }
+
+    fun tie(state: Int) {
+        when
+    }
+
+    fun playerWin(state: Int) {
+
+    }
+
+    fun playerLose(state: Int) {
+
     }
 }
